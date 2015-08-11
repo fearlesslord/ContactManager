3c6ea93096171a3a7ccee47d64bb5d670e4d839e
Taras Hevko <hevkotaras@gmail.com>
2015-08-11 16:32:16 +0200

    add some folders under ignore

--- config/configuration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  //paginator
-  define('ITEMS_PER_PAGE','5');
-  define('PAGE_DIS_PER_RANGE','2');
--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  define('DB_TYPE','mysql');
-  define('DB_HOST','localhost');
-  define('DB_NAME','contact_manager');
-  define('DB_USER','root');
-  define('DB_PASS','');
-
-  define('DRIVER','Mysql');
